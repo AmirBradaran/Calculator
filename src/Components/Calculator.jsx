be07c0ba119c62145e0bedcdbfa3e756ec89c82b
Amir <baradaran13085@gmail.com>
2024-12-08 21:03:31 +0330

    After
    Finally Project

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-
+import './Calculator.css'
 export default function Calculator() {
   const [inp1,setInp1]=useState()
   const [inp2,setInp2]=useState()
   const [resault,setResault]=useState()
-    return (
-    <div>
+  return (
+    <div className="calculate">
       <input
         type="number"
         name="Number"
@@ -22,14 +22,14 @@ export default function Calculator() {
         value={inp2}
         onChange={(e) => setInp2(+e.target.value)}
       />
-      <section>
+      <section className="btn-cal">
         <button onClick={()=>setResault(inp1+inp2)}>+</button>
         <button onClick={()=>setResault(inp1-inp2)}>-</button>
-        <button onClick={()=>setResault(inp1*inp2)}>*</button>
+        <button onClick={()=>setResault(inp1*inp2)}>x</button>
         <button onClick={()=>setResault(inp1/inp2)}>/</button>
         <button onClick={()=>setResault(resault==0)}>Reset</button>
       </section>
-      <p>
+      <p className="resault">
         {resault}
       </p>
     </div>
